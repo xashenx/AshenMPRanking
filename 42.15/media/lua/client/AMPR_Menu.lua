7b063b1fbe4adfc728fd51504ae1fd7d7d82b1f0
Ashen <ashenx@gmail.com>
2026-03-12 17:10:59 +0100

    new version 42.15

---
+++ 42.15/media/lua/client/AMPR_Menu.lua
@@ -0,0 +1,110 @@
+if isServer() then return end;
+
+MenuActions = MenuActions or {}
+
+function MenuActions:onAddUserClick(button)
+    if button.internal == "OK" then
+        -- print("Parent Name IT: " .. button.parent.entry:getInternalText())
+        -- print("Parent Name Placeholder: " .. button.parent.entry:getPlaceholderText())
+        local text = button.parent.entry:getText()
+        if text and text ~= "" then
+            sendClientCommand("AshenMPRanking", "addToRankings", { username = text })
+        end
+    end
+end
+
+function MenuActions:onRemoveUserClick(button)
+    if button.internal == "OK" then
+        local text = button.parent.entry:getText()
+        if text and text ~= "" then
+            sendClientCommand("AshenMPRanking", "removeFromRankings", { username = text })
+        end
+    end
+end
+
+function MenuActions:onA2IClick(button)
+    if button.internal == "OK" then
+        local text = button.parent.entry:getText()
+        if text and text ~= "" then
+            sendClientCommand("AshenMPRanking", "activeToInactive", { username = text })
+        end
+    end
+end
+
+function MenuActions:onI2AClick(button)
+    if button.internal == "OK" then
+        local text = button.parent.entry:getText()
+        if text and text ~= "" then
+            sendClientCommand("AshenMPRanking", "inactiveToActive", { username = text })
+        end
+    end
+end
+
+function MenuActions:onConfirmReset(button)
+    if button.internal == "OK" then
+        print("RESET CONFIRMED: " .. button.parent.entry:getText())
+        local text = button.parent.entry:getText()
+        if text == "YES" then
+            sendClientCommand("AshenMPRanking", "ResetRanking", {})
+        end
+    end
+end
+
+local function doMenu(playerIndex, context, worldobjects, test)
+    local player = getSpecificPlayer(playerIndex)
+    local isadmin = player:getAccessLevel() == "admin"
+    if not isadmin then return true end
+
+    local menu = ISContextMenu:getNew(context)
+    
+    if not menu then return true end
+
+    local opt = context:addOption("Ashen MP Ranking", worldobjects, nil)
+    context:addSubMenu(opt, menu)
+    menu:addOption("Add User", nil, function()
+        local addUseInput = ISTextBox:new(0, 0, 280, 180, "Enter Player Username:", "", MenuActions, MenuActions.onAddUserClick, playerIndex)
+        addUseInput:initialise()
+        addUseInput:addToUIManager()
+    end)
+    menu:addOption("Remove User", nil, function()
+        local removeUseInput = ISTextBox:new(0, 0, 280, 180, "Enter Player Username:", "", MenuActions, MenuActions.onRemoveUserClick, playerIndex)
+        removeUseInput:initialise()
+        removeUseInput:addToUIManager()
+    end)
+    menu:addOption("Show Inactive", nil, function() sendClientCommand("AshenMPRanking", "showInactive", {}) end)
+    menu:addOption("Move User Active2Inactive", nil, function()
+        local a2iInput = ISTextBox:new(0, 0, 280, 180, "Enter Player Username:", "", MenuActions, MenuActions.onA2IClick, playerIndex)
+        a2iInput:initialise()
+        a2iInput:addToUIManager()
+    end)
+    menu:addOption("Move User Inactive2Active", nil, function()
+        local i2aInput = ISTextBox:new(0, 0, 280, 180, "Enter Player Username:", "", MenuActions, MenuActions.onI2AClick, playerIndex)
+        i2aInput:initialise()
+        i2aInput:addToUIManager()
+    end)
+    menu:addOption("Reset Ranking", nil, function()
+        local resetInput = ISTextBox:new(0, 0, 280, 180, "Confirm Reset (type YES):", "", MenuActions, MenuActions.onConfirmReset, playerIndex)
+        resetInput:initialise()
+        resetInput:addToUIManager()
+    end)
+    return true
+end
+
+local onServerResponse = function(module, command, reponseData)
+    -- handles the response from the server
+    if module ~= "AshenMPRanking" or command ~= "ccServerResponse" then
+        return
+    end
+    
+    local text = ""
+    if reponseData.fail_msg ~= nil then
+        text = "*%s*" .. getText(reponseData.fail_msg)
+        processSayMessage(string.format(text, "red", reponseData.username))
+    elseif reponseData.success_msg ~= nil then
+        text = "*%s*" .. getText(reponseData.success_msg)
+        processSayMessage(string.format(text, "green", reponseData.username))
+    end
+end
+
+Events.OnFillWorldObjectContextMenu.Add(doMenu)
+Events.OnServerCommand.Add(onServerResponse)
---
+++ 42.15/media/lua/client/AMPR_Options.lua
@@ -0,0 +1,108 @@
+AshenMPRanking = AshenMPRanking or {}
+AshenMPRanking.Options = AshenMPRanking.Options or {}
+
+AshenMPRanking.Options.receiveData = false
+AshenMPRanking.Options.ladderLength = 2
+AshenMPRanking.Options.Fn = {}
+
+
+if ModOptions and ModOptions.getInstance then
+    local function getHotkeyValue(hotkey)
+        local hotkey_array = {
+            29, -- Left Ctrl
+            56, -- Left Alt
+            157, -- Right Ctrl
+            184, -- Right Alt
+            59, -- F1
+            60, -- F2
+            61, -- F3
+            62, -- F4
+            63, -- F5
+            64, -- F6
+            65, -- F7
+            66, -- F8
+            67, -- F9
+            68, -- F10
+            87, -- F11
+            88, -- F12
+            210, -- Insert
+            211, -- Delete
+            199, -- Home
+            207, -- End
+        }
+        return hotkey_array[hotkey]
+    end
+    
+    local function getTimezoneValue(index)
+        tz = {"-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "+1","+2","+3","+4","+5","+6","+7","+8","+9","+10","+11","+12"}
+        return tonumber(tz[index])
+    end
+
+    local function getLadderLengthValue(index)
+        length_values = {3, 5, 10, 15}
+        return length_values[index]
+    end
+
+    local function onModOptionsApply(optionValues)
+        AshenMPRanking.Options.receiveData = optionValues.settings.options.receiveData
+        AshenMPRanking.Options.ladderLength = getLadderLengthValue(optionValues.settings.options.ladderLength)
+        AshenMPRanking.Options.hotkey = getHotkeyValue(optionValues.settings.options.hotkey)
+        AshenMPRanking.Options.timezone = getTimezoneValue(optionValues.settings.options.timezone)
+    end
+
+    local function onModOptionApplyInGame(optionValues)
+        AshenMPRanking.Options.receiveData = optionValues.settings.options.receiveData
+        AshenMPRanking.Options.ladderLength = getLadderLengthValue(optionValues.settings.options.ladderLength)
+        AshenMPRanking.Options.hotkey = getHotkeyValue(optionValues.settings.options.hotkey)
+        AshenMPRanking.Options.timezone = getTimezoneValue(optionValues.settings.options.timezone)
+        AshenMPRanking.mainUI:setKeyMN(AshenMPRanking.Options.hotkey)
+    end
+
+    local SETTINGS = {
+        options_data = {
+            receiveData = {
+                name = "UI_Options_receive",
+                tooltip = "UI_Options_receive_tooltip",
+                default = false,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionApplyInGame,
+            },
+            ladderLength = {
+                "3", "5", "10", "15",
+                
+                name = "UI_Options_ladderLength",
+                tooltip = "UI_Options_ladderLength_tooltip",
+                default = 2,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionApplyInGame,
+            },
+            hotkey = {
+                "L-CTRL", "L-ALT", "R-CTRL", "R-ALT", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "INSERT", "DELETE", "HOME", "END",
+    
+                name = "UI_Options_hotkey",
+                tooltip = "UI_Options_hotkey_tooltip",
+                default = 2,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionApplyInGame,
+            },
+            timezone = {
+                "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "+1", "+2", "+3", "+4", "+5", "+6", "+7", "+8", "+9", "+10", "+11", "+12",
+                
+                name = "UI_Options_timezone",
+                tooltip = "UI_Options_timezone_tooltip",
+                default = 13,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionApplyInGame,
+            },
+        },
+
+        mod_id = 'AshenMPRanking',
+        mod_shortname = 'Ashen MP Ranking',
+        mod_fullname = 'Ashen MP Ranking',
+    }
+    
+    local optionsInstance = ModOptions:getInstance(SETTINGS)
+    ModOptions:loadFile()
+
+    Events.OnPreMapLoad.Add(function() onModOptionsApply({ settings = SETTINGS }) end)
+end
